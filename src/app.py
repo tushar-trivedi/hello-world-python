a05d2aac8b437be548ec9c0a343a7adc09d17eaa
Tushar Trivedi <tushar.trivedi@sap.com>
2025-02-24 09:55:41 +0530

    check3

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World Tushar-Trivedi !'
+    return 'HELLO WORLD!'
 
 if __name__ == '__main__':
     http_server = WSGIServer(("", 8080), app)
