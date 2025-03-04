6d1e7d47da069761e5d5c0ef6f2fec412e944cfc
Tushar Trivedi <tushar.trivedi@sap.com>
2025-03-04 10:11:56 +0530

    basic1

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HELLO WORLD!'
+    return 'HELLO DEVLOPER!'
 
 if __name__ == '__main__':
     http_server = WSGIServer(("", 8080), app)
