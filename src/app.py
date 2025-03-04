af88bea1ef9d04c0c6064f19ff144904ad87a3a7
Tushar Trivedi <tushar.trivedi@sap.com>
2025-03-04 22:03:04 +0530

    test1

--- src/app.py
+++ src/app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'HELLO World'
 
-@app.route('/thankyou/<name>')
-def thankyou_world(name):
-    return f"Thankyou {name}"
-
 if __name__ == '__main__':
     http_server = WSGIServer(("", 8080), app)
     http_server.serve_forever()
