72c1e55c0f1380d2206eb30b4ea693a749720e9f
Tushar Trivedi <tushar.trivedi@sap.com>
2025-03-04 14:54:12 +0530

    added sonar check

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HELLO DEVLOPER!'
+    return 'HELLO SONARQUBE!'
 
 if __name__ == '__main__':
     http_server = WSGIServer(("", 8080), app)
