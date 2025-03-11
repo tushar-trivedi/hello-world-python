17d81cb59f7daa15dafcb167a48d0e4e1046dac9
Tushar Trivedi <tushar.trivedi@sap.com>
2025-03-11 18:03:02 +0530

    test

--- src/app.py
+++ src/app.py
@@ -5,11 +5,8 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', message='HELLO DEVOPS')
+    return render_template('index.html', message='HELLO WORLD')
 
-@app.route('/thankyou/<name>')
-def thank_you(name):
-    return render_template('index.html', message=f'Thank You, {name}!')
 
 if __name__ == '__main__':
     http_server = WSGIServer(("", 8080), app)
